0e4eb98a4cce93c98255d9c41e8d843db3c842b1
Vrypel <144292053+Vrypel@users.noreply.github.com>
2023-11-17 21:32:05 -0500

    Changed how the leaderboard class works

--- src/main/java/com/example/cs208_assignment4/Leaderboard.java
+++ src/main/java/com/example/cs208_assignment4/Leaderboard.java
@@ -1,41 +1,62 @@
 package com.example.cs208_assignment4;
 
 import java.util.Hashtable;
-import java.util.Map;
 
 public class Leaderboard {
 
-    private Hashtable<Integer, Integer> damageTable;
+    private Hashtable scoreTable; // Table for storing scores
 
     public Leaderboard() {
-        damageTable = new Hashtable<>();
+        scoreTable = new Hashtable(10); // Initialize the score table with initial capacity
     }
 
-    public void addDamage (Entity entity, int damage) {
-        int entityCode = entity.hashCode();
-        damageTable.put(entityCode, damageTable.getOrDefault(entityCode, 0) + damage);
+    public void updateScore(Entity player, int score) {
+        int playerCode = player.hashCode();
+        scoreTable.put(playerCode, score);
     }
 
-    public int getDamage(Entity entity) {
-        return damageTable.getOrDefault(entity.hashCode(), 0);
-
+    public int getScore(Entity player) {
+        int playerCode = player.hashCode();
+        return (int) scoreTable.get(playerCode);
     }
-
-    public void reset() {
-        damageTable.clear();
-    }
-
-    public void displayLeaderboard(Controller controller) {
-        damageTable.entrySet().stream()
-                .sorted(Map.Entry.<Integer, Integer>comparingByValue().reversed())
-                .forEach(entry -> {
-                    Entity entity = controller.findEntityByHashCode(entry.getKey());
-                    if (entity != null) {
-                        System.out.println(entity.name + ": " + entry.getValue() + " damage");
-                    } else {
-                        System.out.println("Unknown Entity: " + entry.getValue() + " damage");
-                    }
-                });
-    }
-
 }
+
+/*
+*
+ public class Leaderboard {
+
+ private Hashtable<Integer, Integer> damageTable;
+
+ public Leaderboard() {
+ damageTable = new Hashtable<>();
+ }
+
+ public void addDamage (Entity entity, int damage) {
+ int entityCode = entity.hashCode();
+ damageTable.put(entityCode, damageTable.getOrDefault(entityCode, 0) + damage);
+ }
+
+ public int getDamage(Entity entity) {
+ return damageTable.getOrDefault(entity.hashCode(), 0);
+
+ }
+
+ public void reset() {
+ damageTable.clear();
+ }
+
+ public void displayLeaderboard(Controller controller) {
+ damageTable.entrySet().stream()
+ .sorted(Map.Entry.<Integer, Integer>comparingByValue().reversed())
+ .forEach(entry -> {
+ Entity entity = controller.findEntityByHashCode(entry.getKey());
+ if (entity != null) {
+ System.out.println(entity.name + ": " + entry.getValue() + " damage");
+ } else {
+ System.out.println("Unknown Entity: " + entry.getValue() + " damage");
+ }
+ });
+ }
+
+ }
+ */
