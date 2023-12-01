f710af5e50312d16496fc3ffa063c06a90cd633a
Victor Serra <vserra3192@gmail.com>
2023-12-01 11:41:53 -0500

    Big Commit, Added 2 new buttons (Add Player Button and Difficulty Button) which is used before clicking the start game button to set up the round.

--- src/main/java/com/example/cs208_assignment4/Controller.java
+++ src/main/java/com/example/cs208_assignment4/Controller.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 import java.util.*;
 
@@ -17,7 +18,16 @@ import java.util.*;
  * @author Christopher Duran
  */
 public class Controller {
-
+    @FXML
+    public Button addPlayerButton;
+    @FXML
+    public TextField newPlayerTextField;
+    @FXML
+    public Label diceLabel;
+    @FXML
+    public Button difficultyButton;
+    @FXML
+    public Label difficultyLabel;
     @FXML
     private Label player_name;
 
@@ -49,6 +59,7 @@ public class Controller {
     private Button startGame_button;
 
     private LinkedList<Player> playerList;
+    private ArrayList<String> playerNameList;
 
     private LinkedList<Player> lobby;
 
@@ -65,60 +76,63 @@ public class Controller {
             "Goblin",
             "Troll",
             "Giant",
-            "Dragon"
+            "Evil Knight",
+            "Skeleton King",
+            "Dragon",
+            "Demi-gorgon"
     };
 
     private Iterator<Player> iterator;
-
     private Leaderboard leaderboard;
+    private int playerCount = 1;
+    private int maxLevels;
 
 
     /**
-     * Main constructor
-     * Initializes list of players and one enemy object
+     * Method initializes the GUI components, playerList, lobby and leaderboard before
+     * running the application to avoid any NullExceptions. Also sets starting level
+     * number and initial enemy.
      * @author David Arzumanyan
      */
-    public Controller() {
+    public void initialize() {
+        // Setting the text area as non-editable
+        this.textArea.setEditable(false);
+        this.newPlayerTextField.setStyle("-fx-background-color: white;");
+        this.textArea.setText("^Add New Players Above^ \n");
+
         this.playerList = new LinkedList<Player>();
+        this.playerNameList = new ArrayList<>();
         this.lobby = new LinkedList<Player>();
         this.leaderboard = new Leaderboard();
 
         // Initializes level counter
         this.levelNum = 1;
 
-        // Adding players
-        this.playerList.add(new Player("David", 100, 100, 20));
-        this.playerList.add(new Player("Victor", 100, 100, 20));
-        this.playerList.add(new Player("Christopher", 100, 100, 20));
-
-
-        // Setting the current player and the enemy
-        this.current_player = this.playerList.getFirst();
+        // Setting the enemy
         this.enemy = new Enemy(enemyNames[0], 50, 100, 55);
-
         this.welcomeStatus = 0;
-
-        this.iterator = this.playerList.iterator();
     }
 
     /**
-     * Method initializes the GUI components before
-     * running the application to avoid any NullExceptions
-     * @author David Arzumanyan
+     * Controls what happens when addPlayer button is pressed. Adds a new player with the name typed within
+     * {@code newPlayerTextField} and appends text stating that it was added to {@code playerList}. If nothing
+     * is typed and addPlayer is pressed, "Invalid Player Name" message is appended.
+     * @param event defines the event which happens at every click on the button
+     * @author Victor Serra
      */
-    public void initialize() {
-        // Setting the text area as non-editable
-        this.textArea.setEditable(false);
-
-        // Initialize labels and set their text
-        this.player_name.setText("Player: " + this.current_player.name);
-        this.player_health.setText("Health: " + this.current_player.health);
-        this.player_armor.setText("Armor: " + this.current_player.armor);
-
-        this.enemy_name.setText("Enemy: " + this.enemy.name);
-        this.enemy_health.setText("Health: " + this.enemy.health);
-        this.enemy_armor.setText("Armor: " + this.enemy.armor);
-
+    @FXML
+    private void onAddPlayer(ActionEvent event){
+        //If no name is typed or name is already chosen print "Invalid Player Name"
+        if(this.newPlayerTextField.getText().equals("") || playerNameList.contains(this.newPlayerTextField.getText())){
+            this.textArea.appendText("Invalid Player Name.\n");
+        }else { //else add Player to list with chosen name
+            this.playerList.add(new Player(this.newPlayerTextField.getText(), 100, 100, 25));
+            this.playerNameList.add(this.newPlayerTextField.getText());
+            this.textArea.appendText(this.newPlayerTextField.getText() + " was added to party! (Total PLayers: " + playerCount + ")\n");
+            this.playerCount++;
+            this.newPlayerTextField.clear();
+            this.newPlayerTextField.setPromptText("New Player Name");
+        }
     }
 
     /**
@@ -183,24 +197,88 @@ public class Controller {
      */
     @FXML
     public void startGame() {
-        if (this.welcomeStatus == 0) {
-            // Displaying the first message - players and enemy information
-            this.textArea.appendText("Welcome, the game has started!\n");
-            this.textArea.appendText("Below listed players are playing against the " + this.enemy.name + "\n");
-            for (Player player : this.playerList) {
-                this.textArea.appendText("Player: " + player.name + "\n");
+        //Sets the attributes for the game depending on the difficulty
+        String difficulty = this.difficultyButton.getText();
+        if(difficulty.equals("Easy")) { //Players start with an abundant amount of health and a lot of damage
+            this.maxLevels = 3; //only 3 enemies
+            for(Player p: playerList){
+                p.health = 200;
+                p.setDamagePerTurn(75);
             }
-            this.welcomeStatus += 1;
-        }
-        else if (this.welcomeStatus == 1){
-            this.textArea.appendText("The game is already in progress, playing players are `\n");
-            for (Player player : this.playerList) {
-                this.textArea.appendText("Player: " + player.name + "\n");
+        } else if (difficulty.equals("Normal")) {   //Players start with a good amount of health and good damage
+            this.maxLevels = 5; //5 enemies
+            for(Player p: playerList){
+                p.health = 125;
+                p.setDamagePerTurn(35);
+            }
+        } else if (difficulty.equals("Hard")) {  //Players start with standard amount of health and ok damage
+            this.maxLevels = 8; //8 enemies
+            for(Player p: playerList){
+                p.setDamagePerTurn(25);
             }
         }
-        else{
-            this.textArea.appendText("The game is over!\n");
+        //checks if any players have been created
+        if (playerList.size()!=0) {
+            //Hides initial elements
+            this.newPlayerTextField.setVisible(false);
+            this.addPlayerButton.setVisible(false);
+            this.difficultyButton.setVisible(false);
+            this.difficultyLabel.setVisible(false);
+
+            //Show invisible elements
+            this.enemy_name.setVisible(true);
+            this.enemy_health.setVisible(true);
+            this.enemy_armor.setVisible(true);
+            this.diceLabel.setVisible(true);
+            this.rolled_number.setVisible(true);
+            this.player_health.setVisible(true);
+            this.player_armor.setVisible(true);
+
+            // Sets current player as first player added
+            this.current_player = this.playerList.getFirst();
+            this.iterator = this.playerList.iterator();
+
+            // Initialize labels and set their text
+            this.player_name.setText("Player: " + this.current_player.name);
+            this.player_health.setText("Health: " + this.current_player.health);
+            this.player_armor.setText("Armor: " + this.current_player.armor);
+
+            this.enemy_name.setText("Enemy: " + this.enemy.name);
+            this.enemy_health.setText("Health: " + this.enemy.health);
+            this.enemy_armor.setText("Armor: " + this.enemy.armor);
+
+            if (this.welcomeStatus == 0) {
+                // Displaying the first message - players and enemy information
+                this.textArea.appendText("Welcome, the game has started!\n");
+                this.textArea.appendText("The game is on " + this.difficultyButton.getText() + " difficulty\n");
+                this.textArea.appendText("Below listed players are playing against the " + this.enemy.name + "\n");
+                for (Player player : this.playerList) {
+                    this.textArea.appendText("Player: " + player.name + "\n");
+                }
+                this.welcomeStatus += 1;
+            } else if (this.welcomeStatus == 1) {
+                this.textArea.appendText("The game is already in progress, playing players are `\n");
+                for (Player player : this.playerList) {
+                    this.textArea.appendText("Player: " + player.name + "\n");
+                }
+            } else {
+                this.textArea.appendText("The game is over!\n");
+            }
+        }else { //if no players have been created
+            this.textArea.appendText("You must add players to start the game\n");
         }
+    }
+
+    /**
+     * Flips though three difficulty texts on {@code difficultyButton} (Easy, Normal, Hard),
+     * which is used to determine the difficulty.
+     * @param event
+     * @author Victor Serra
+     */
+    public void onDifficultyButton(ActionEvent event) {
+        if(this.difficultyButton.getText().equals("Easy")){ this.difficultyButton.setText("Normal");
+        } else if (this.difficultyButton.getText().equals("Normal")) { this.difficultyButton.setText("Hard");
+        } else if (this.difficultyButton.getText().equals("Hard")) { this.difficultyButton.setText("Easy"); }
     }
 
     /**
@@ -211,7 +289,6 @@ public class Controller {
      * @author Victor Serra
      */
     public void nextLevel(){
-        int maxLevels = 5;
         if (levelNum != maxLevels) {
             //increment level counter
             this.levelNum += 1;
@@ -223,13 +300,14 @@ public class Controller {
             this.enemy.armor = 50 * levelNum;
             this.enemy_health.setText("Health: " + enemy.health);
             this.enemy_armor.setText("Armor: " + enemy.armor);
+            //increase Player stats by a small amount
             for(Player p: playerList){
-                p.health += 15;
-                p.armor += 5;
                 double damageCounter = p.damagePerTurn * 1.6;
+                p.armor += 10;
                 p.setDamagePerTurn((int) (damageCounter));
             }
         }else{
+            //game over
             this.textArea.appendText("=========GAME OVER=========\nAll Enemies were defeated!\n");
             this.enemy_health.setText("Health: 0");
             this.enemy_armor.setText("Armor: 0");
@@ -342,7 +420,7 @@ public class Controller {
                 // Setting the player's health label
                 if (this.current_player.health <= 0){
                     this.player_health.setText("Health: 0");
-                    this.textArea.appendText("Player: " + this.current_player.name + " defeated!\n");
+                    this.textArea.appendText("  -Player: " + this.current_player.name + " defeated!\n");
                     this.lobby.add(this.current_player);
                     this.playerList.remove(this.current_player);
 
